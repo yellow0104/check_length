0a7b544c09c08c685abd6b23956db917462ad8a6
위정우 <80455444+yellow0104@users.noreply.github.com>
2022-07-28 21:30:21 +0900

    main
    간단하게 만들어봤습니다

---
+++ len/main.py
@@ -0,0 +1,22 @@
+
+from tkinter import END, Button, ttk
+import tkinter as tk
+
+from click import command
+
+win = tk.Tk()
+
+scroll_bar = tk.Text(width=30, height=15)
+scroll_bar.pack()
+
+def lencommand():
+    scroll_len = len(scroll_bar.get(1.0, END))-1
+    len_lable.config(text=scroll_len)
+
+button_len = Button(text='길이확인', command=lencommand)
+button_len.pack()
+
+len_lable = tk.Label(text='0')
+len_lable.pack()
+
+win.mainloop()
